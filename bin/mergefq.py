9a107ed9badd1c46efe1d150bb608a723f6d205a
vbrendel <vbrendel@indiana.edu>
2020-12-23 19:33:55 -0500

    mergefq.py to python3

--- bin/mergefq.py
+++ bin/mergefq.py
@@ -5,7 +5,6 @@
 #  is printed to files of chunksize read pairs each, named outflable-mergedreads-number.
 #
 from Bio import SeqIO
-import itertools
 import sys
 import os
 
@@ -15,7 +14,7 @@ def merge_fastq(fastq_path1, fastq_path2, outflabel, chunksize):
     fnlabel = 0
     fastq_iter1 = SeqIO.parse(open(fastq_path1),"fastq")
     fastq_iter2 = SeqIO.parse(open(fastq_path2),"fastq")
-    for rec1, rec2 in itertools.izip(fastq_iter1, fastq_iter2):
+    for rec1, rec2 in zip(fastq_iter1, fastq_iter2):
         i += 1
         if i%chunksize == 0 :
            if i > 0 :
